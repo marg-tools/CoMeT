a8e6005b892c3229e3a60c16f3c6cddd51634c08
Rajesh Kedia <kedia.rajesh@gmail.com>
2021-03-03 10:46:11 +0530

    added sampling rate support for viedeo

--- scripts/heatView.sh
+++ scripts/heatView.sh
@@ -3,11 +3,12 @@
 script_name=$0
 trace_file=$1
 png_folder=$2
+sampling_rate=$3
 
 python_script="$(dirname "$script_name" )/heatView.py"
 
 mkdir -p $png_folder
-python $python_script $trace_file $png_folder
+python $python_script $trace_file $png_folder $sampling_rate
 
 cd $png_folder
 ffmpeg -framerate 1 -i heatmap_%d.png -start_number 1 -c:v mpeg4 -vtag xvid -qscale:v 4 -c:a libmp3lame -qscale:a 5 $trace_file.avi
