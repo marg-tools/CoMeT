5f36b80f08cbb37783b3d5810b33974e49a55168
Rajesh Kedia <kedia.rajesh@gmail.com>
2021-03-22 16:32:05 +0530

    updated patches for parsec

--- benchmarks/parsec/patches/huge_val.sh
+++ benchmarks/parsec/patches/huge_val.sh
@@ -2,5 +2,5 @@
 
 # Fix HUGE related error.
 grep -rl "HUGE" parsec-2.1/pkgs/apps/ferret | xargs sed -i "s/HUGE/HUGE_VAL/g"
-grep -rl "HUGE" parsec-2.1/pkgs/netapps/netferret | xargs sed -i "s/HUGE/HUGE_VAL/g"
+#grep -rl "HUGE" parsec-2.1/pkgs/netapps/netferret | xargs sed -i "s/HUGE/HUGE_VAL/g"
 
