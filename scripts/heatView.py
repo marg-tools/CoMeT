01a0e74d07e9baef5a307f2b104a0dca54fe6c8f
Rajesh Kedia <kedia.rajesh@gmail.com>
2021-04-11 23:34:25 +0530

    video script configurations exposed as command line arguments

--- scripts/heatView.py
+++ scripts/heatView.py
@@ -5,35 +5,163 @@ from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm
 from numpy import linspace
 import sys
+import math
+import getopt
 from matplotlib import gridspec
+import shutil, os
 
 import numpy as np
+
+
+def usage():
+    print "ERror"
+
+cores_in_x=4
+cores_in_y=4
+cores_in_z=1
+
+banks_in_x=4
+banks_in_y=4
+banks_in_z=8
+
+arch_type="2D"        ##option = 3D, other.
+inverted_view=False
+
+plot_type="3D"          ##option = 2D, 3D
+MAX_ROWS_2D_PLOT=4
+
+#for 3D plot
+layer_to_view=0
+type_to_view="MEMORY"          #CORE, MEMORY
+
+
+debug=False
+verbose= False
+
+tfilename = 'temperature.trace'
+pngFolder = 'maps'
+samplingRate = 1
+tmin = 65.0
+tmax = 81.0
+cleanDir=False
+
+
+if not sys.argv[1:]:
+  usage()
+  sys.exit()
+
+opts_passthrough = [ 'cores_in_x=', 'cores_in_y=', 'cores_in_z=', 'banks_in_x=', 'banks_in_y=', 'banks_in_z=', 'arch_type=', 'plot_type=', 'layer_to_view=', 'type_to_view=', 'verbose', 'inverted_view', 'debug', 'tmin=', 'tmax=', 'samplingRate=', 'traceFile=', 'output=', 'clean' ]
+
+try:
+        #                     arguments,  shortopts,  longopts
+  opts, args = getopt.getopt(sys.argv[1:], "hvidcs:t:o:", opts_passthrough)
+except getopt.GetoptError, e:
+  # print help information and exit:
+  print e
+  usage()
+  sys.exit()
+for o, a in opts:
+  if o == '-h':
+    usage()
+    sys.exit()
+  if o == '-v' or o == '--verbose':
+    verbose = True
+  if o == '-d' or o == '--debug':
+    debug = True
+  if o == '-i' or o == '--inverted_view':
+    inverted_view = True
+  if o == '--cores_in_x':
+    cores_in_x = a
+  if o == '--cores_in_y':
+    cores_in_y = a
+  if o == '--cores_in_z':
+    cores_in_z = a
+  if o == '--banks_in_x':
+    banks_in_x = a
+  if o == '--banks_in_y':
+    banks_in_y = a
+  if o == '--banks_in_z':
+    banks_in_z = a
+  if o == '--arch_type':
+    arch_type = a
+  if o == '--plot_type':
+    plot_type = a
+  if o == '--layer_to_view':
+    layer_to_view = int(a)
+  if o == '--type_to_view':
+    type_to_view = a
+  if o == '-s' or o == '--samplingRate':
+    samplingRate = int(a)
+  if o == '-t' or o == '--traceFile':
+    tfilename = a
+  if o == '-o' or o == '--output':
+    pngFolder = a
+  if o == '--tmin':
+    tmin = float(a)
+  if o == '--tmax':
+    tmax = float(a)
+  if o == '-c' or o == '--clean':
+    cleanDir = True
+
+if (debug):
+    print "Command line options:"
+    print opts
+
+
+
+
 
 # %matplotlib inline
 
 #temperatures = [73.41,73.71,73.69,73.39,73.72,74.04,74.03,73.69,73.69,74.04,74.01,73.66,73.36,73.68,73.67,73.34,72.05,72.36,72.35,72.04,72.37,72.70,72.69,72.35,72.34,72.70,72.67,72.32,72.01,72.33,72.32,71.99,70.12,70.44,70.43,70.11,70.45,70.80,70.78,70.43,70.42,70.79,70.77,70.41,70.08,70.42,70.40,70.07,67.63,67.96,67.95,67.62,67.97,68.34,68.33,67.96,67.94,68.33,68.32,67.93,67.59,67.94,67.93,67.58,64.59,64.94,64.94,64.58,64.96,65.35,65.34,64.94,64.93,65.34,65.34,64.92,64.56,64.93,64.91,64.55,61.03,61.41,61.40,61.03,61.42,61.85,61.84,61.40,61.40,61.84,61.84,61.39,61.00,61.39,61.39,61.00,56.98,57.39,57.39,56.98,57.40,57.87,57.86,57.38,57.38,57.86,57.86,57.38,56.97,57.38,57.38,56.96,52.48,52.92,52.92,52.49,52.93,53.43,53.43,52.91,52.91,53.43,53.42,52.91,52.48,52.92,52.91,52.47]
-temperatures = [0] * 128
+total_cores = cores_in_x*cores_in_y*cores_in_z
+total_banks = banks_in_x*banks_in_y*banks_in_z
 
-def parse_tfile(tfilename, index, orig_line):
-    position=index
+temperatures_core = [0] * total_cores
+temperatures_bank = [0] * total_banks
+
+def parse_tfile(tfilename, orig_line):
     line=orig_line.rstrip()
     line=line.split('\t')
     i = 0
-    for value in line[position:]:
-        temperatures[i] = float(value)
+    if (debug):
+        print ("DEBUG::Temperature trace:\n%s" %line)
+    for value in line[0:total_cores]:
+        temperatures_core[i] = float(value)
+        i+=1
+    i = 0
+    for value in line[total_cores:]:
+        temperatures_bank[i] = float(value)
         i+=1
 
 def parse_tfile_header(tfilename, orig_line):
-    position=0
+    count_core_column=0
+    count_bank_column=0
+    column=0
     line=orig_line.split('\t')
-    while (line[position].startswith('LC')):
-        position+=1
-    return position
+    if (debug):
+        print ("DEBUG::Temperature trace file header:\n%s" %line)
+
+    while (line[column].startswith('C')):
+        count_core_column+=1
+        column+=1
+    while (line[column].startswith('B')):
+        count_bank_column+=1
+        column+=1
+    if (count_core_column!=total_cores):
+        print("WARNING: Total core columns does not match the number of cores!!")
+        print("    Number cores:%d, number columns:%d" %(total_cores, count_core_column))
+    if (count_bank_column!=total_banks):
+        print("WARNING: Total bank columns does not match the number of banks!!")
+        print("    Number banks:%d, number columns:%d" %(total_banks, count_bank_column))
 
 
 def plot_opaque_cube(ax, x=10, y=20, z=30, dx=40, dy=50, dz=60, color='cyan'):
 
-    kwargs = {'alpha': 1, 'color': color}
+    if (debug):
+        print("DEBUG:: plot_opaque_cube: x=%d,y=%d,z=%d" %(x,y,z))
+
+    kwargs = {'alpha': 1, 'color': color, 'edgecolor': 'k'}
 
     xx = np.linspace(x, x+dx, 2)
     yy = np.linspace(y, y+dy, 2)
@@ -60,101 +188,253 @@ def plot_opaque_cube(ax, x=10, y=20, z=30, dx=40, dy=50, dz=60, color='cyan'):
     #plt.title("Cube")
     #plt.show()
 
-
-
-
-if __name__ == "__main__":
+def plot_3D_structure(ax, tmin, tmax, xdim, ydim, zdim, zstart, xwidth, ywidth, temperatures, title_message, axes_postprocess=True, inverted=False, layer_type="Core"):
+    if (debug):
+        print ("DEBUG:: plot_3D_structure: xwidth = %f" %xwidth)
     color_lvl = 200
-    color=cm.RdYlBu(linspace(1.0, 0.0, color_lvl+1))
-    tfilename = str(sys.argv[1])
-    pngFolder = str(sys.argv[2])
-    try:
-        samplingRate = int(sys.argv[3])
-    except:
-        samplingRate = 1
-    fp = open(tfilename, 'r')
-    lines = fp.readlines()
-    index = parse_tfile_header(tfilename, lines[0])
-    count = 0
-    for line in lines[1:]:
-        if (count%samplingRate != 0):
-            count+=1
-            continue
+    shift_amount = 0  #earlier 0.2
+    color=cm.RdYlBu_r(linspace(0.0, 1.0, color_lvl+1))
+    temp_range = tmax - tmin
+    quantum = temp_range / color_lvl
+    ind=0
+    if (inverted):
+        zrange = reversed(range(zdim))
+    else:
+        zrange = range(zdim)
+    for zz in zrange:
+        for xx in range(xdim):
+            for yy in range(ydim):
+                offset = temperatures[ind] - tmin
+                offset = int(offset/quantum)
+                if offset < 0:
+                    offset = 0
+                if offset > color_lvl:
+                    offset = color_lvl
+                plot_opaque_cube(ax, 0+xwidth*xx, 0+ywidth*yy-shift_amount*(zz+zstart), 0+zz+zstart, xwidth, ywidth, 0.2, color[offset])
+                ind += 1
+        if (inverted):
+            layer_num = zdim-zz-1
+        else:
+            layer_num = zz
+        annotate_text = layer_type + " L"+ str(layer_num)
+        ax.text(xx+2,-0.6,zz+zstart+0.5,annotate_text,(0,1,0), verticalalignment='center')
+#        ax.annotate("Time step = "+str(count) +" ms", xy=(xx+1, 0), xycoords='axes points', fontsize=19)
 
-        fig = plt.figure()
-        gs = gridspec.GridSpec(1, 2, width_ratios=[4, 1]) 
-        ax = fig.add_subplot(gs[0], projection='3d')
-        parse_tfile(tfilename, index, line)
-        tmin = 65.0
-        tmax = 81.0
-        #tmin = min(temperatures)
-        #tmax = max(temperatures)
-        temp_range = tmax - tmin
-        quantum = temp_range / color_lvl
-        count+=1
-        for zz in range(8):
-            for xx in range(4):
-                for yy in range(4):
-                    ind = yy + 4*xx + 4*4*zz
-                    offset = temperatures[ind] - tmin
-                    offset = int(offset/quantum)
-                    if offset < 0:
-                        offset = 0
-                    if offset > color_lvl:
-                        offset = color_lvl
-                    plot_opaque_cube(ax, 0+xx, 0+yy-0.2*zz, 0+zz, 1, 1, 0.2, color[offset])
-
+    if (axes_postprocess==True):        #true only for the last plot in 3D.
         yint = []
         locs, labels = plt.yticks()
         for each in locs:
             yint.append(int(each))
         plt.yticks(yint)
-
+    
         yint = []
         locs, labels = plt.xticks()
         for each in locs:
             yint.append(int(each))
         plt.xticks(yint)
-        plt.title('Time step: '+str(count)+' ms')
-        plt.annotate("8 layer, 4x4 HMC stack", xy=(0.1, 0.8), xycoords='figure fraction')
-        plt.annotate("Top layer", xy=(0.08, 0.7), xycoords='figure fraction')
-        plt.annotate("Bottom layer", xy=(0.08, 0.2), xycoords='figure fraction')
-        #ax.view_init(210,70)   #reversed
-        ax.view_init(10,20)
+        plt.title(title_message, y=-0.01, fontsize=19)
+        if (xdim > ydim):
+            ax.set_ylim(0, xdim)
+        elif (ydim > xdim):
+            ax.set_xlim(0, ydim)
+
+        ##ax.view_init(210,70)   #reversed
+        if (zdim+zstart < 5):
+            ax.set_zlim3d(bottom=0.0, top=5)
+        ax.view_init(10,20)     #elevation, azimuthal
         plt.axis('off')
+        #plt.show()
+                
 
 
-        ##Adding the bottom layer view
-        ax = fig.add_subplot(gs[1])
-        b_temp = np.random.random(( 4 , 4 )) 
-        index=0
-        for i in range(4):
-            for j in range(4):
-                b_temp[i][j] = temperatures[index]
-                index+=1
-        cmap = mpl.cm.RdYlBu_r
-        norm = mpl.colors.Normalize(vmin=tmin, vmax=tmax)
-        #print(b_temp)
-        plt.imshow( b_temp , cmap = cmap , norm=norm, interpolation = 'nearest', aspect='equal', origin='lower') 
-        #yint = []
-        #locs, labels = plt.yticks()
-        #print(locs)
-        #for each in locs:
-        #    yint.append(int(each))
-        #plt.yticks(yint)
-        #print(yint)
+def plot_2D_map(ax, tmin, tmax, xdim, ydim, xwidth, ywidth, title_message, temperatures):
+    if (debug):
+        print("DEBUG:: plot_2D_map: xdim=%d,ydim=%d,temperatures=%s" %(xdim,ydim,temperatures))
 
-        #yint = []
-        #locs, labels = plt.xticks()
-        #print(locs)
-        #for each in locs:
-        #    yint.append(int(each))
-        #plt.xticks(yint)
-        #print(yint)
-        plt.yticks([0,1,2,3])
-        plt.xticks([0,1,2,3])
-        plt.annotate("Bottom layer\ncross section", xy=(0.73, 0.65), xycoords='figure fraction')
+    cmap = mpl.cm.RdYlBu_r
+    norm = mpl.colors.Normalize(vmin=tmin, vmax=tmax)
+    arr = np.array(temperatures)
+    arr1 = arr.reshape(xdim, ydim)
+    #plt.imshow( arr1 , cmap = cmap , norm=norm, interpolation = 'nearest', aspect='equal', origin='upper') 
+    plt.pcolormesh( arr1, cmap = cmap, norm=norm, edgecolor='k') 
+    #plt.grid(color='k', linewidth=2)
+    plt.axis('off')
+    plt.title(title_message, fontsize=19)
+    ax.set_aspect('equal')
+    ax.invert_yaxis()
 
+
+
+
+##main function. Script begins from here
+if __name__ == "__main__":
+        #    parse_args()
+#    tfilename = str(sys.argv[1])
+#    pngFolder = str(sys.argv[2])
+#    try:
+#        samplingRate = int(sys.argv[3])
+#    except:
+#        samplingRate = 1
+#    try:
+#        tmin = float(sys.argv[4])
+#        tmax = float(sys.argv[5])
+#    except:
+#        tmin = 65.0
+#        tmax = 81.0
+
+    print("\n")
+    print("Received temperature filename: %s" %tfilename)
+    print("Received output directory name: %s" %pngFolder)
+    print("Setting sampling rate: %d" %samplingRate)
+    print("Setting min. temperature limit (blue color): %.1f" %tmin)
+    print("Setting max. temperature limit (red color) : %.1f" %tmax)
+    print("\n")
+    if not os.path.exists(pngFolder):
+        os.mkdir(pngFolder)
+    elif (cleanDir):
+        shutil.rmtree(pngFolder)
+        os.mkdir(pngFolder)
+    fp = open(tfilename, 'r')
+    lines = fp.readlines()
+    parse_tfile_header(tfilename, lines[0])         #pass first line to process
+    count = 0                   #count of line number
+    for line in lines[1:]:
+        if (count%samplingRate != 0):
+            count+=1
+            continue
+        fig = plt.figure(figsize=(14, 8))
+
+        if (plot_type == "2D"):
+            max_banks = max(banks_in_z, cores_in_z)
+            rows = min(MAX_ROWS_2D_PLOT, max_banks)
+            cols_banks = int(math.ceil(float(banks_in_z)/rows))
+            cols_cores = int(math.ceil(float(cores_in_z)/rows))
+
+            gs = gridspec.GridSpec(nrows=rows, ncols=cols_cores+cols_banks) 
+        elif (arch_type == "3D"):
+            gs = gridspec.GridSpec(nrows=1, ncols=2, width_ratios=[3, 1]) 
+        else:
+            gs = gridspec.GridSpec(nrows=1, ncols=3, width_ratios=[1, 2, 0.7]) 
+
+        parse_tfile(tfilename, line)
+        if (debug):
+            print("\n")
+            print("DEBUG::Core temperatures:", temperatures_core)
+            print("DEBUG::Bank temperatures:", temperatures_bank)
+        print("Processing line %d" %count)
+
+        count+=1
+        if (plot_type == "2D"):
+            for j in range(cores_in_z):
+                ax = fig.add_subplot(gs[j%MAX_ROWS_2D_PLOT, j/MAX_ROWS_2D_PLOT])
+                xdim = cores_in_x
+                ydim = cores_in_y
+                start_index = j * xdim * ydim
+                end_index = start_index + xdim * ydim
+                temperatures = temperatures_core[start_index : end_index]
+                title_message = "CORE layer " + str(j)
+                plot_2D_map(ax, tmin, tmax, xdim, ydim, 1, 1, title_message, temperatures)
+
+            for j in range(banks_in_z):
+                ax = fig.add_subplot(gs[j%MAX_ROWS_2D_PLOT, cols_cores + j/MAX_ROWS_2D_PLOT])
+                xdim = banks_in_x
+                ydim = banks_in_y
+                start_index = j * xdim * ydim
+                end_index = start_index + xdim * ydim
+                temperatures = temperatures_bank[start_index : end_index]
+                title_message = "MEMORY layer " + str(j)
+                plot_2D_map(ax, tmin, tmax, xdim, ydim, 1, 1, title_message, temperatures)
+
+        #beginning of 3D plotting
+        else:
+            ax = fig.add_subplot(gs[0], projection='3d')
+                        #ind = yy + cores_in_y*xx + cores_in_x*cores_in_y*zz
+
+            #core
+            if (arch_type=="3D"):
+                xwidth=(float)(banks_in_x)/cores_in_x
+                ywidth=(float)(banks_in_y)/cores_in_y
+                if (inverted_view):
+                    zstart = 0
+                else:
+                    zstart = banks_in_z
+                postprocess=False
+                title_message = None
+            else:
+                xwidth=1
+                ywidth=1
+                zstart = 0
+                postprocess=True
+                title_message = "Core temperature map"
+            plot_3D_structure(ax, tmin, tmax, cores_in_x, cores_in_y, cores_in_z, zstart, xwidth, ywidth, temperatures_core, title_message, postprocess, inverted_view, "Core ")
+
+            #memory
+            if (arch_type=="3D"):
+                postprocess=True
+                title_message = "3D architecture temperature map"
+                if (inverted_view):
+                    zstart = cores_in_z
+                else:
+                    zstart = 0
+            else:
+                postprocess=True
+                title_message = "Memory temperature map"
+                ax = fig.add_subplot(gs[1], projection='3d')
+            xwidth=1
+            ywidth=1
+            plot_3D_structure(ax, tmin, tmax, banks_in_x, banks_in_y, banks_in_z, zstart, xwidth, ywidth, temperatures_bank, title_message, postprocess, inverted_view, "Mem.")
+            arch_string = "Core: " + str(cores_in_x) + "x" + str(cores_in_y) + "x" + str(cores_in_z) + ", "
+            arch_string += "Memory: "+ str(banks_in_x) + "x" + str(banks_in_y) + "x" + str(banks_in_z) 
+            plt.annotate(arch_string, xy=(0.13, 0.93), xycoords='figure fraction', fontsize=16)
+            plt.annotate("Time step = "+str(count) +" ms", xy=(0.13, 0.85), xycoords='figure fraction', fontsize=19)
+            if (inverted_view):
+                #plt.axhline(0.03, color='r')
+                plt.annotate("Heat Sink at bottom", xy=(0.13, 0.15), xycoords='figure fraction', fontsize=15)
+            else:
+                #plt.axhline(0.85, color='r')
+                plt.annotate("Heat Sink at top", xy=(0.13, 0.75), xycoords='figure fraction', fontsize=15)
+
+
+
+
+
+##            ##Adding the bottom layer view
+            
+            if (arch_type=="3D"):
+                ax = fig.add_subplot(gs[1])
+            else:
+                ax = fig.add_subplot(gs[2])
+
+
+            if (type_to_view == "CORE"):
+                if (layer_to_view >= cores_in_z):
+                    print ("\nERROR:: INVALID layer_to_view parameter. Should be less than number of core layers\n")
+                    exit()
+                xdim = cores_in_x
+                ydim = cores_in_y
+                start_index = layer_to_view * xdim * ydim
+                end_index = start_index + xdim * ydim
+                temperatures = temperatures_core[start_index : end_index]
+                title_message = "CORE layer " + str(layer_to_view)
+            elif (type_to_view == "MEMORY"):
+                if (layer_to_view >= banks_in_z):
+                    print ("\nERROR:: INVALID layer_to_view parameter. Should be less than number of memory layers\n")
+                    exit()
+                xdim = banks_in_x
+                ydim = banks_in_y
+                start_index = layer_to_view * xdim * ydim
+                end_index = start_index + xdim * ydim
+                temperatures = temperatures_bank[start_index : end_index]
+                title_message = "MEMORY layer " + str(layer_to_view)
+            else:
+                print ("\nERROR:: INVALID type_to_view parameter. Should be CORE or MEMORY\n")
+                exit()
+
+            plot_2D_map(ax, tmin, tmax, xdim, ydim, 1, 1, title_message, temperatures)
+
+        #plt.annotate("Core layer", xy=(0.08, 0.7), xycoords='figure fraction')
+        #plt.annotate("Memory layer", xy=(0.08, 0.65), xycoords='figure fraction')
+        #plt.annotate("Memory layer", xy=(0.08, 0.2), xycoords='figure fraction')
 
 
 
@@ -186,3 +466,39 @@ if __name__ == "__main__":
         plt.close(fig)
 
     fp.close()
+
+os.system("cd " + pngFolder)
+image_files=pngFolder+"/heatmap_%d.png"
+video_file=pngFolder+"/output.avi"
+os.system("ffmpeg -framerate 1 -i " + image_files + " -start_number 1 -c:v mpeg4 -vtag xvid -qscale:v 4 -c:a libmp3lame -qscale:a 5 " + video_file)
+
+##        b_temp = np.random.random(( 4 , 4 )) 
+##        index=0
+##        for i in range(4):
+##            for j in range(4):
+##                b_temp[i][j] = temperatures[index]
+##                index+=1
+##        cmap = mpl.cm.RdYlBu_r
+##        norm = mpl.colors.Normalize(vmin=tmin, vmax=tmax)
+##        #print(b_temp)
+##        plt.imshow( b_temp , cmap = cmap , norm=norm, interpolation = 'nearest', aspect='equal', origin='lower') 
+##        #yint = []
+##        #locs, labels = plt.yticks()
+##        #print(locs)
+##        #for each in locs:
+##        #    yint.append(int(each))
+##        #plt.yticks(yint)
+##        #print(yint)
+##
+##        #yint = []
+##        #locs, labels = plt.xticks()
+##        #print(locs)
+##        #for each in locs:
+##        #    yint.append(int(each))
+##        #plt.xticks(yint)
+##        #print(yint)
+##        plt.yticks([0,1,2,3])
+##        plt.xticks([0,1,2,3])
+##        plt.annotate("Bottom layer\ncross section", xy=(0.73, 0.65), xycoords='figure fraction')
+##
+##
