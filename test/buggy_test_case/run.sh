f51df2a5cda267071b88a6b55f6d64455972efc6
Shailja Pandey <shailey1993@gmail.com>
2021-07-05 12:30:14 +0530

    Minor edits

--- test/buggy_test_case/run.sh
+++ test/buggy_test_case/run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-for (( c=1; c<=32; c++ ))
+for (( c=1; c<=4; c++ ))
 do
         mknod test${c}_pipe.sift p
 done
 
-for (( c=1; c<=32; c++ ))
+for (( c=1; c<=4; c++ ))
 do
    ../../record-trace -o test${c}_pipe --roi -- ./test -p1 &
 done
