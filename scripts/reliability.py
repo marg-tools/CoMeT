66d1e5a4c6a8693cac7756e4966ce0bf6051e8cd
JorisoptenBerg <jjpoptenberg@gmail.com>
2023-05-24 15:56:43 +0200

    Fixed some subcore-related issues and improved code clarity

--- scripts/reliability.py
+++ scripts/reliability.py
@@ -7,14 +7,14 @@ import sys, os, sim
 # get general config options
 output_dir = sim.config.get('general/output_dir')
 vth = float(sim.config.get('power/vth'))
-delta_v_scale_factor = float(sim.config.get('reliability/delta_v_scale_factor'))
 vdd_file = 'InstantVdd.log'
 
 # get reliability config options
-enabled             = True if sim.config.get('reliability/enabled') =='true' else False
-separate_traces     = True if sim.config.get('reliability/separate_core_mem_trace') =='true' else False
-reliability_exec    = os.path.join(os.getenv('SNIPER_ROOT'), sim.config.get('reliability/reliability_executable'))
-acceleration_factor = sim.config.get('reliability/acceleration_factor')
+enabled              = True if sim.config.get('reliability/enabled') =='true' else False
+separate_traces      = False if sim.config.get('core_power/tp') =='true' else True
+acceleration_factor  = sim.config.get('reliability/acceleration_factor')
+delta_v_scale_factor = float(sim.config.get('reliability/delta_v_scale_factor'))
+reliability_exec     = os.path.join(os.getenv('SNIPER_ROOT'), sim.config.get('reliability/reliability_executable'))
 
 # combined files
 comb_temperature_trace_file = sim.config.get('hotspot/log_files/combined_insttemperature_trace_file')
@@ -44,16 +44,12 @@ def write_headers(trace_file, full_trace_file, state_file, delta_v_file, header)
     header_len = len(header.split('\t'))
     with open(trace_file, "w") as f:
         f.write("%s\n" %(header))
-    f.close()
     with open(full_trace_file, "w") as f:
         f.write("%s\n" %(header))
-    f.close()
     with open(state_file, "w") as f:
         f.write("0.0\t"*header_len+"\n")
-    f.close()
     with open(delta_v_file, "w") as f:
         f.write("0.0\t"*header_len+"\n")
-    f.close()
 
 def init_reliability_files(combined_header, ptrace_header):
     # Initialise separate trace files
@@ -65,6 +61,8 @@ def init_reliability_files(combined_header, ptrace_header):
     write_headers(m_rvalue_trace_file, m_full_rvalue_trace_file, m_state_file, m_delta_v_file, ptrace_header)
 
     # Initialise combined trace files
+    if separate_traces:
+        combined_header += 'tmp\t'*80 # TODO: dont hardcode this!
     write_headers(comb_instant_trace_file, comb_periodic_trace_file, comb_state_file, comb_delta_v_file, combined_header)
 
 def clean_reliability_files():
@@ -78,19 +76,17 @@ def clean_reliability_files():
 def target_freqs(freqs):
     with open(comb_delta_v_file, 'r') as file:
         delta_v = map(lambda x: float(x), file.readline().strip().split('\t'))
-    file.close()
     with open(vdd_file, 'r') as file:
         file.readline() # ignore header
         vdd = map(lambda x: float(x), file.readline().strip().split('\t'))
-    file.close()
 
-    # based on equation (1) in Rathore (2019) LifeGuard: A Reinforcement 
-    # Learning-Based Task Mapping Strategy for Performance-Centric Aging 
-    # Management
-    def new_f(f):
+    # based on equation (1) from
+    # Rathore et al. (2019): LifeGuard: A Reinforcement Learning-Based Task 
+    # Mapping Strategy for Performance-Centric Aging Management
+    def new_f(f, i):
         return f / (1 - (delta_v[i] * delta_v_scale_factor)/(vdd[i] - vth))
 
-    return [new_f(f) for i, f in enumerate(freqs)]
+    return [new_f(f, i) for i, f in enumerate(freqs)]
 
 def execute_reliability(delta_t_ms, timestamp_ms, temperature_trace_file, vdd_trace_file, state_file, delta_v_file, instant_trace_file, periodic_trace_file):
     # Setup call to reliability binary `reliability_external`.
@@ -99,7 +95,7 @@ def execute_reliability(delta_t_ms, timestamp_ms, temperature_trace_file, vdd_tr
             vdd_trace_file, state_file, delta_v_file, instant_trace_file, 
             acceleration_factor)
 
-    print("DEBUG: executing: {}".format(reliability_cmd))
+    print("[Reliability]: executing {}".format(reliability_cmd))
     os.system(reliability_cmd)
 
     # Copy current rvalues to periodic log.
@@ -108,24 +104,47 @@ def execute_reliability(delta_t_ms, timestamp_ms, temperature_trace_file, vdd_tr
         with open(periodic_trace_file, 'a') as rvalues:
             rvalues.write(current_rval.readline())
 
+def write_vdd_file(vdd_filename, reference_file, value, mode):
+    with open(reference_file, 'r') as ref:
+        ref.readline() # ignore header
+        data_len = len(ref.readline().split('\t'))
+    with open(vdd_file, 'r') as current_vdd:
+        with open(vdd_filename, 'w') as new_vdd:
+            header = current_vdd.readline()
+            data = current_vdd.readline().strip()
+            header_len = len(header.split('\t'))
+            new_vdd.write(header)
+            if mode == 'combined':
+                new_vdd.write(data+'\t')
+                for _ in range(data_len-header_len):
+                    new_vdd.write(value + '\t')
+            elif mode == 'core':
+                for _ in range(data_len):
+                    new_vdd.write(data.split('\t')[0] + '\t')
+            elif mode == 'mem':
+                for _ in range(data_len):
+                    new_vdd.write(value + '\t')
+            else: 
+                raise(Exception('Invalid mode in call to function write_vdd_file'))
+            new_vdd.write('\n')
+    return vdd_filename
+
 def update_reliability_values(delta_t, timestamp):
     # Update the reliability values of the cores.
-    # Wearout is calculated using on the temperatures in the file
-    # `instant_temperatures` (in celsius) over the time period `delta_t` (in
-    # seconds)
 
     delta_t_ms = delta_t/sim.util.Time.MS
     timestamp_ms = timestamp/sim.util.Time.MS
 
+    comb_vdd_file = write_vdd_file('combined_InstantVdd.log', comb_temperature_trace_file, '0.8', 'combined')
+    execute_reliability(delta_t_ms, timestamp_ms, comb_temperature_trace_file,
+                        comb_vdd_file, comb_state_file, comb_delta_v_file,
+                        comb_instant_trace_file, comb_periodic_trace_file)
     if separate_traces:
-        # TODO: modify vdd_file for subcore components
+        c_vdd_file = write_vdd_file('InstantVdd_core.log', c_temperature_trace_file, '0.8', 'core')
+        m_vdd_file = write_vdd_file('InstantVdd_mem.log', m_temperature_trace_file, '0.8', 'mem')
         execute_reliability(delta_t_ms, timestamp_ms, c_temperature_trace_file,
-                            vdd_file, c_state_file, c_delta_v_file,
+                            c_vdd_file, c_state_file, c_delta_v_file,
                             c_rvalue_trace_file, c_full_rvalue_trace_file)
         execute_reliability(delta_t_ms, timestamp_ms, m_temperature_trace_file,
-                            vdd_file, m_state_file, m_delta_v_file,
+                            m_vdd_file, m_state_file, m_delta_v_file,
                             m_rvalue_trace_file, m_full_rvalue_trace_file)
-    else:
-        execute_reliability(delta_t_ms, timestamp_ms, comb_temperature_trace_file,
-                            vdd_file, comb_state_file, comb_delta_v_file,
-                            comb_instant_trace_file, comb_periodic_trace_file)
